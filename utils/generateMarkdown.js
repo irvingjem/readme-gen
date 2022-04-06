caa2d15cfb7ac82bb438785e613ca419a07f04e5
Your Name <irving.jem@gmail.com>
2022-04-06 11:23:04 -0700

    readme generating per requirements

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,23 +9,23 @@ function renderLicenseBadge(license) {
 };
 
 
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
 
 ## Description:
     ${data.description}
-
     ${renderLicenseBadge(data.license)}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contrubuting](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
 
 ## Installation Instructions: 
     ${data.install}
@@ -33,12 +33,27 @@ function generateMarkdown(data) {
 ## Usage Instructions: 
     ${data.usage}
 
+## Contributors:
+    ${data.contributors}    
+
 ## Tests:
     ${data.test}
 
-## Contributors:
-    ${data.contributors}
+## Questions
+    If there are any questions about this project feel free to email me at ${data.email}. Find all of my other projects at https://github.com/${data.github}.
 `;
 }
 
+
+// Exporting markdown to the index
 module.exports = generateMarkdown;
+
+
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+//function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+//function renderLicenseSection(license) {}
